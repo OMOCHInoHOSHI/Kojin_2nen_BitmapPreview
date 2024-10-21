1c82b3b4f22b480d24848a21e7f6b330b3069310
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2024-10-22 00:06:45 +0900

    カメラアイコンを撮影ボタンに

--- app/src/main/java/com/example/myapplicationtest/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.kt
@@ -6,17 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.rounded.PhotoCamera
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chaquo.python.Python.getInstance
 import com.example.myapplicationtest.ui.theme.MyApplicationTestTheme
 
@@ -40,8 +49,24 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier.padding(innerPadding)   //余白
                         )
                     }
-                    //カメラ権限呼び出し
-                    CameraScreen()
+
+                    var flg by remember { mutableIntStateOf(0) } // flg の状態を管理する
+                    FilledTonalButton(
+                        onClick = { flg = 1 },
+                        modifier = Modifier.size(80.dp).padding(1.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.PhotoCamera, // カメラのアイコンに変更
+                            contentDescription = "カメラ起動",
+                            modifier = Modifier.size(ButtonDefaults.IconSize)
+                        )
+                        //Text(text = "カメラ起動")
+                    }
+                    if(flg == 1){
+                        //カメラ権限呼び出し
+                        CameraScreen()
+                    }
+
                 }
             }
         }
@@ -84,13 +109,27 @@ fun Greeting(name: String,
 @Composable
 fun GreetingPreview() {
     MyApplicationTestTheme {
-        var str by remember { mutableStateOf("あんどろ") }
+//        var str by remember { mutableStateOf("あんどろ") }
+//
+////        var moji="ボタンがタップされました-"
+//        var moji= stringResource(id=R.string.app_name)
+//
+//        Box {
+//            Greeting(str, onClick = {str = moji})
+//        }
 
-//        var moji="ボタンがタップされました-"
-        var moji= stringResource(id=R.string.app_name)
+        var flg by remember { mutableIntStateOf(0) } // flg の状態を管理する
 
-        Box {
-            Greeting(str, onClick = {str = moji})
+        FilledTonalButton(
+            onClick = { flg = 1 },
+            modifier = Modifier.size(80.dp).padding(8.dp)
+
+        ) {
+            Text(text = "カメラ起動")
+        }
+        if(flg == 1){
+            //カメラ権限呼び出し
+            CameraScreen()
         }
 
     }
