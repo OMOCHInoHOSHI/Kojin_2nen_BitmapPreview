c48eece5d34e18111fb4c9ab9f892b881acc9fba
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2024-11-17 22:36:45 +0900

    撮影ボタンの配置変更

--- app/src/main/java/com/example/myapplicationtest/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.kt
@@ -2,56 +2,43 @@ package com.example.myapplicationtest
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Camera
-import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.rounded.Camera
+import androidx.compose.material.icons.rounded.Photo
 import androidx.compose.material.icons.rounded.PhotoCamera
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chaquo.python.Python.getInstance
 import com.example.myapplicationtest.ui.theme.MyApplicationTestTheme
-import androidx.activity.compose.setContent
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
-import android.content.res.AssetManager
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.rounded.Photo
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
 
@@ -80,6 +67,7 @@ class MainActivity : ComponentActivity() {
 //                    }
 
                     //カメラ起動S------------------------------------------------------------
+
                     var camera_flg by remember { mutableIntStateOf(0) }
                     Box(
                         modifier = Modifier
@@ -263,7 +251,6 @@ fun PhotoPicker(onPickPhoto: () -> Unit) {
         Text(text = "写真を選択")
     }
 }
-
 
 
 //プレビューのためのテストコードS----------------------------------------------------
--- app/src/main/java/com/example/myapplicationtest/TakePhoto.kt
+++ app/src/main/java/com/example/myapplicationtest/TakePhoto.kt
@@ -1,6 +1,9 @@
 package com.example.myapplicationtest
 
 import android.widget.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Camera
@@ -10,19 +13,34 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.w3c.dom.Text
 
 //TakePhotoコンポーザはボタンのUIを表示する
 @Composable
 fun TakePhoto(takePhoto:() -> Unit){
-    //takePhotoは、ラムダの引数を受け取る
-    Button(onClick = takePhoto) {
-        Text(text = "撮影")
-        Icon(
-            imageVector = Icons.Rounded.Camera, // カメラのアイコンに変更
-            contentDescription = "カメラ起動",
-            modifier = Modifier.size(ButtonDefaults.IconSize)
-        )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        //takePhotoは、ラムダの引数を受け取る
+        Button(onClick = takePhoto,
+        modifier = Modifier
+            .align(Alignment.BottomCenter)// 下部中央に配置
+            .padding(bottom = 50.dp) // 下部からの余白
+        ) {
+            Text(text = "撮影")
+            Icon(
+                imageVector = Icons.Rounded.Camera, // カメラのアイコンに変更
+                contentDescription = "カメラ起動",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+        }
     }
+
 }
+
